349f06d01980ca3985f8fb144b6259c7462ff4bd
Shivani Tawade <tawadeshivani20@gmail.com>
2025-03-01 21:45:59 +0530

    edited

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
   return (
     <div className="flex items-center space-x-3">
-      <div className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36">
+      <div className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 xl:w-48 xl:h-48">
         <img 
           src="/images/image(1).jpg" 
           alt="Golden Guardians Logo" 
