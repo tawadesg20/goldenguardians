52facaec85ed53879f63f1d490511b3826e89ffc
Shivani Tawade <tawadeshivani20@gmail.com>
2025-03-01 21:37:01 +0530

    adjust logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
     <div className="flex items-center space-x-3">
       <div className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-40 lg:h-40">
         <img 
-          src="/images/logo_update.jpg" 
+          src="/images/image(1).jpg" 
           alt="Golden Guardians Logo" 
           className="w-full h-full object-contain"
          
