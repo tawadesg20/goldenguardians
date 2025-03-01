7ac11da6669fb9a333761baeae66b64c3b05e8f5
Shivani Tawade <tawadeshivani20@gmail.com>
2025-03-01 21:39:57 +0530

    logo.jsx edited

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,16 +1,34 @@
+// import React from 'react';
+
+// const Logo = () => {
+//   return (
+//     <div className="flex items-center space-x-3">
+//       <div className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-40 lg:h-40">
+//         <img 
+//           src="/images/image(1).jpg" 
+//           alt="Golden Guardians Logo" 
+//           className="w-full h-full object-contain"
+         
+//         />
+    
+//       </div>
+//     </div>
+//   );
+// };
+
+
+// export default Logo;
 import React from 'react';
 
 const Logo = () => {
   return (
     <div className="flex items-center space-x-3">
-      <div className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-40 lg:h-40">
+      <div className="w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64">
         <img 
           src="/images/image(1).jpg" 
           alt="Golden Guardians Logo" 
           className="w-full h-full object-contain"
-         
         />
-    
       </div>
     </div>
   );
